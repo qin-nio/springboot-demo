54932ab1cea4e7531e9cd43827afe99b01914ddf
Ling <a@b>
2021-12-10 12:36:33 +0800

    last commit 2

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>11 222 333 world";
+        return "hello K8s <br/>11 222 333 hello world";
     }
 }
